4d7963f3cfe44264b6a4d10d0f605020b72a7bf0
Erik L. Arneson <earneson@arnesonium.com>
2015-11-30 14:23:32 -0800

    Basic singleton class and config loader

--- a3h.php
+++ a3h.php
@@ -28,3 +28,46 @@ License: GPLv2 or later
 
 defined('ABSPATH') or die("No script kiddies please!");
 
+/**
+ * Main PonoRez interface class
+ *
+ * @class PonoRez
+ */
+final class PonoRez {
+    /**
+     * @var PonoRez The single instance of the class
+     * @since 2.1
+     */
+    protected static $_instance = null;
+
+    /**
+     * @var array Configuration values
+     */
+    protected $_config = null;
+
+    public function __construct () {
+        // Load private configuration.
+        $this->_config = json_decode(file_get_contents("private.json"));
+    }
+    
+    /**
+     * @return PonoRez - Singleton instance
+     * @see PR()
+     */
+    public static function instance () {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+}
+
+/**
+ * Returns the main instance of the PonoRez object.
+ *
+ * @return PonoRez
+ */
+function PR() {
+        return PonoRez::instance();
+}
+
