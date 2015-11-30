075dd7254ea181d273573314475a3b7767783e8d
Erik L. Arneson <earneson@arnesonium.com>
2015-11-30 13:59:33 -0800

    Initial plugin files
    This doesn't do anything yet, but the files are here.

---
+++ a3h.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package A3H
+ */
+/*
+Plugin Name: A3H PonoRez Reservation Interface for WordPress
+Description: 
+Version: 0.1
+Author: Erik L. Arneson
+Author URI: http://www.arnesonium.com/
+License: GPLv2 or later
+
+    Copyright 2015 Activities & Attractions Association of Hawaii, Inc.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+defined('ABSPATH') or die("No script kiddies please!");
+
---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== A3H PonoRez Reservation Interface for WordPress ===
+Contributors: arnesonium
+Tags: a3h, vacation, ponorez, rentals, reservation, hawaii
+Requires at least: 4.0
+Tested up to: 4.3.1
+Stable tag: v0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Add A3H PonoRez interfaces, reservations, and tours to your WordPress site.
+
+== Description ==
+
+
+= Usage =
+
+
+== Installation ==
+
+To install this plugin, follow these directions:
+
+1. Download the latest zip file.
+1. Next, load up your WordPress blog’s dashboard, and go to **Plugins > Add New**.
+1. Upload the zip file.
+1. Click **Activate**.
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+== Changelog ==
+= 0.1 =
+* Development pre-release.
+
+== Upgrade Notice ==
+
+
