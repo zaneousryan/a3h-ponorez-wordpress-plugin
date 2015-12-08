422dee4db71e5bb84fb5076f1ea9d7cccc6f23e1
Erik L. Arneson <earneson@arnesonium.com>
2015-12-07 16:09:50 -0800

    Cleaning up templates
    Templates expanded, more shortcodes added. Guest types still can't be
    built automatically. Need to discuss further.

--- lib/class-ponoreztemplate.php
+++ lib/class-ponoreztemplate.php
@@ -44,13 +44,29 @@ final class PonoRezTemplate {
         return $rval . do_shortcode($template_string);
     }
 
+    public function prActivityNameShortcode ($atts = array(), $content = null, $tag) {
+        $a = shortcode_atts(array(
+            'id' => null
+        ), $atts);
+
+        return $this->_currentActivity->name;
+    }
+
+    public function prActivityDescriptionShortcode ($atts = array(), $content = null, $tag) {
+        $a = shortcode_atts(array(
+            'id' => null
+        ), $atts);
+
+        return $this->_currentActivity->description;
+    }
+    
     public function prDatepickerShortcode ($atts = array(), $content = null, $tag) {
         $a = shortcode_atts(array(
             'id' => null
         ), $atts);
 
         $rval_template = <<<EOT
-<input id="date_aXXXX" onclick="calendar(XXXX, 'date_aXXXX', false);" readOnly size="15"> <a onMouseOver="window.status='Date Picker';return true;" onMouseOut="window.status='';return true;" href="javascript:calendar(XXXX, 'date_aXXXX', false);"> <img height="22" src="https://www.hawaiifun.org/reservation/company/show-calendar.gif" width="24" border="0"></a>
+<input id="date_aXXXX" onclick="calendar(XXXX, 'date_aXXXX', false);" readOnly size="15"> <a onMouseOver="window.status='Date Picker';return true;" onMouseOut="window.status='';return true;" href="javascript:calendar(XXXX, 'date_aXXXX', false);"><img class="activityDatePicker" height="22" src="https://www.hawaiifun.org/reservation/company/show-calendar.gif" width="24" border="0"></a>
 EOT;
 
         $rval = str_replace('XXXX', $this->_currentActivity->id, $rval_template);
@@ -58,7 +74,7 @@ EOT;
         return $rval;
     }
 
-    public function prGuestTypesShortcode($atts = array(), $content = null, $tag) {
+    public function prGuestTypeListShortcode($atts = array(), $content = null, $tag) {
         $a = shortcode_atts(array(
             'id' => null
         ), $atts);
@@ -73,7 +89,7 @@ EOT;
                                                         'date' => new SoapVar(date('YYYY-MM-DD'), XSD_DATE)));
         }
         catch (SoapFault $e) {
-            $rval = sprintf("<pre>\n%s\n---\n%s\n</pre>\n",
+            $rval = sprintf("<h3>[SOAP FAULT] Could not load guest types</h3>\n<pre>\n%s\n---\n%s\n</pre>\n",
                             $e->faultcode,
                             $e->faultstring);
 
@@ -96,6 +112,29 @@ EOT;
         return $rval;
     }
 
+    // @TODO For this to be done automatically, we might need to configure it in the DB.
+    public function prGuestTypeShortcode ($atts = array(), $content = null, $tag) {
+        $a = shortcode_atts(array(
+            'id' => null,
+            'guest-type-id' => null,
+            'max' => 20
+        ), $atts);
+
+        $rval = sprintf('<select class="guestCount%d" guest-type-id="%d" id="guests_a%d_t%d">',
+                        $this->_currentActivity->id,
+                        $a['guest-type-id'],
+                        $this->_currentActivity->id,
+                        $a['guest-type-id']);
+
+        for ($i = 0; $i <= $a['max']; $i++) {
+            $rval .= sprintf("<option value='%d'>%d</option>", $i, $i);
+        }
+
+        $rval .= "</select>\n";
+
+        return $rval;
+    }
+    
     public function prHotelSelectShortcode ($atts = array(), $content = null, $tag) {
         $a = shortcode_atts(array(
             'id' => null
@@ -139,8 +178,11 @@ EOT;
 
     public function registerShortcodes() {
         add_shortcode('pr_activity', array($this, 'prActivityShortcode'));
+        add_shortcode('pr_activity_name', array($this, 'prActivityNameShortcode'));
+        add_shortcode('pr_activity_description', array($this, 'prActivityDescriptionShortcode'));
         add_shortcode('pr_datepicker', array($this, 'prDatepickerShortcode'));
-        add_shortcode('pr_guest_types', array($this, 'prGuestTypesShortcode'));
+        add_shortcode('pr_guest_type_list', array($this, 'prGuestTypeListShortcode'));
+        add_shortcode('pr_guest_type', array($this, 'prGuestTypeShortcode'));
         add_shortcode('pr_hotel_select', array($this, 'prHotelSelectShortcode'));
         add_shortcode('pr_hotel_room', array($this, 'prHotelRoomShortcode'));
         add_shortcode('pr_check_availability', array($this, 'prCheckAvailabilityShortcode'));
